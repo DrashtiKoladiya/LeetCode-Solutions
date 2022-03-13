f9850201afda672844efe41a28ef5785bd7993ab
Drashti Koladiya <58821355+DrashtiKoladiya@users.noreply.github.com>
2022-03-13 12:40:13 +0530

    Time: 100 ms (93.88%), Space: 61.4 MB (26.04%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    // differant ways to solve this problem
+    // 1. slow-fast pointer technique
+    // 2. using binary search with the concept of Pigeonhole principle
+    // 3. normal hashing
+    // 4. by making number at ith index negative 
     int findDuplicate(vector<int>& nums) {
         
         int n = nums.size(),slow,fast,prev;
