cf689c74ebb68a060e0b5d1e4e09dd3e3d26967d
Drashti Koladiya <58821355+DrashtiKoladiya@users.noreply.github.com>
2022-06-01 00:30:45 +0530

    Time: 52 ms (42.46%), Space: 13.1 MB (77.85%) - LeetHub

---
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        int mx=INT_MIN,mn=INT_MAX;
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                mn = min(mn,matrix[i][j]);
+                mx = max(mx,matrix[i][j]);
+            }
+        }
+        
+        while(mn<=mx)
+        {
+            int mid = mn + (mx-mn)/2;
+            int cnt1=0, cnt2=0;
+            
+            
+            for(int i=0;i<n;i++)
+            {
+                cnt1+= upper_bound(matrix[i].begin(),matrix[i].end(),mid-1)-matrix[i].begin();
+                cnt2+= (m - (upper_bound(matrix[i].begin(),matrix[i].end(),mid) - matrix[i].begin()));
+            }
+            //cout<<k<<"\n";
+            //cout<<mn<<" "<<mx<<"\n";
+            //cout<<mid<<" "<<cnt1<<" "<<cnt2<<"\n";
+            
+            if(cnt1<k)
+            {
+                if(cnt2<=(n*m-k)) return mid;
+                else
+                {
+                    mn = mid+1;
+                    //k-= cnt1;
+                }
+            }
+            else
+            {
+                mx = mid-1;
+            }
+        }
+        
+        return -1;
+        
+    }
+};
