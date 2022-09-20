055dc13552ec450ff6d1f53c87e9729a93b3a7f0
Drashti Koladiya <58821355+DrashtiKoladiya@users.noreply.github.com>
2022-09-21 00:27:01 +0530

    Time: 260 ms (88.73%), Space: 11.8 MB (97.92%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -5,7 +5,7 @@ public:
         int n1 = nums1.size();
         int n2 = nums2.size();
         
-        int dp[n1+1][n2+1], ans=0;
+        int dp[2][n2+1], ans=0;
         memset(dp,0,sizeof(dp));
         
         for(int i=0;i<=n1;i++)
@@ -14,16 +14,16 @@ public:
             {
                 if(i==0 || j==0)
                 {
-                    dp[i][j]=0;
+                    dp[i%2][j]=0;
                 }
                 else if(nums1[i-1]==nums2[j-1])
                 {
-                    dp[i][j] = dp[i-1][j-1] + 1;
-                    ans = max(ans, dp[i][j]);
+                    dp[i%2][j] = dp[(i-1)%2][j-1] + 1;
+                    ans = max(ans, dp[i%2][j]);
                 }
                 else
                 {
-                    dp[i][j] = 0;
+                    dp[i%2][j] = 0;
                 }
             }
         }
